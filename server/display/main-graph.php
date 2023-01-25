f3987d74f07386b054fa0a9c9a172a2f5265af8a
SuninDarknes <sunindark00@gmail.com>
2023-01-25 16:55:20 +0100

    update

--- server/display/main-graph.php
+++ server/display/main-graph.php
@@ -10,16 +10,4 @@ $sql_query = "SELECT * FROM PROSTORIJA";
 $result = mysqli_query($con, $sql_query);
 print_r(mysqli_fetch_array($result));
 echo "<br>";
-
-$sql_query = "ALTER TABLE PROSTORIJA RENAME COLUMN ADRESA TO NAZIV";
-$result = mysqli_query($con, $sql_query);
-print_r(mysqli_fetch_array($result));
-echo "<br>";
-
-$sql_query = "SELECT * FROM PROSTORIJA";
-$result = mysqli_query($con, $sql_query);
-print_r(mysqli_fetch_array($result));
-
-
-
 ?>
