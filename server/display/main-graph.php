27db0de06dde530cc6b0affa422d2096ae121447
SuninDarknes <sunindark00@gmail.com>
2023-01-25 20:43:08 +0100

    Dovršeni main-graf

--- server/display/main-graph.php
+++ server/display/main-graph.php
@@ -1,5 +1,7 @@
 <?php 
 include "..//config.php";
+
+$debug = false;
 if(!isset($_GET["prostorija"])){
     echo "U zahtjevu nema informacije o adresi.\n";
     exit();
@@ -8,33 +10,29 @@ if(!isset($_GET["prostorija"])){
 $sql_query = "SELECT * FROM PROSTORIJA WHERE NAZIV='".$_GET["prostorija"]."'";
 $result = mysqli_query($con, $sql_query);
 $data = mysqli_fetch_array($result);
-echo $sql_query;
-echo "<br>";
-print_r($data);
-echo "<br>";
+if($debug) echo $sql_query;
+if($debug) echo "<br>";
+if($debug)print_r($data);
+if($debug) echo "<br>";
 
 
-//Uzimanje podataka o temperaturama
+//Uzimanje podataka o senzorima temperatura
 $sql_query = "SELECT * FROM TEMP_SENZOR WHERE ID_CVOR=".$data["ID_CVOR"];
 $result = mysqli_query($con, $sql_query);
-echo $sql_query;
-echo "<br>";
+if($debug) echo $sql_query;
+if($debug) echo "<br>";
 
 while ($row = mysqli_fetch_array($result)){
-    if($row["TIP"] == 0)$zrakID = $row["ID"];
+    if($row["TIP"] == 0)$sobnaID = $row["ID"];
     if($row["TIP"] == 1)$radijatorID = $row["ID"];
     if($row["TIP"] == 2)$kilmaID = $row["ID"];
 }
 
-
-
-
-
-//Uzimanje podataka o prozorima
+//Uzimanje podataka o senzoru prozora
 $sql_query = "SELECT * FROM STATUSOBJEKT_SENZOR WHERE ID_CVOR=".$data["ID_CVOR"];
 $result = mysqli_query($con, $sql_query);
-echo $sql_query;
-echo "<br>";
+if($debug) echo $sql_query;
+if($debug) echo "<br>";
 
 while ($row = mysqli_fetch_array($result)){
     if($row["TIP"] == 1)$prozorID = $row["ID"];
@@ -42,14 +40,58 @@ while ($row = mysqli_fetch_array($result)){
 
 
 
+
+
+
+
+
+//Čitanje i spremanje podataka o sobnoj temperaturi
+$sql_query = "SELECT * FROM TEMP WHERE ID_SENZOR=".$sobnaID;
+$result = mysqli_query($con, $sql_query);
+if($debug) echo $sql_query;
+$dataset="[";
+while ($row = mysqli_fetch_array($result)){
+    $dataset.= "{x:'".$row["VRIJEME"]."',y:'".$row["VRIJEDNOST"]."'},";
+}
+$sobnaData= rtrim(str_replace("\n", "",$dataset), ",")."]";
+
+
+//Čitanje i spremanje podataka o temperaturi radijatora
+$sql_query = "SELECT * FROM TEMP WHERE ID_SENZOR=".$radijatorID;
+$result = mysqli_query($con, $sql_query);
+if($debug) echo $sql_query;
+$dataset="[";
+while ($row = mysqli_fetch_array($result)){
+    $dataset.= "{x:'".$row["VRIJEME"]."',y:'".$row["VRIJEDNOST"]."'},";
+}
+$radijatorData= rtrim(str_replace("\n", "",$dataset), ",")."]";
+
+
+//Čitanje i spremanje podataka o temperaturi kilme
+$sql_query = "SELECT * FROM TEMP WHERE ID_SENZOR=".$kilmaID;
+$result = mysqli_query($con, $sql_query);
+if($debug) echo $sql_query;
+$dataset="[";
+while ($row = mysqli_fetch_array($result)){
+    $dataset.= "{x:'".$row["VRIJEME"]."',y:'".$row["VRIJEDNOST"]."'},";
+}
+$klimaData= rtrim(str_replace("\n", "",$dataset), ",")."]";
+
+
+
+
+
+
+//Čitanje i spremanje podataka o stanju prozora
 $sql_query = "SELECT * FROM STATUSOBJEKT WHERE ID_SENZOR=".$prozorID;
 $result = mysqli_query($con, $sql_query);
-echo $sql_query;
+if($debug) echo $sql_query;
 $dataset="[";
 while ($row = mysqli_fetch_array($result)){
     $dataset.= "{x:'".$row["VRIJEME"]."',y:'".(($row["VRIJEDNOST"]==0 ) ? "ZATVOREN" : "OTVOREN")."'},";
 }
-$prozorDataset= rtrim(str_replace("\n", "",$dataset), ",")."]";
+$prozorData= rtrim(str_replace("\n", "",$dataset), ",")."]";
+
 ?>
 
 <div>
@@ -59,6 +101,9 @@ $prozorDataset= rtrim(str_replace("\n", "",$dataset), ",")."]";
 <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/chartjs-adapter-date-fns/dist/chartjs-adapter-date-fns.bundle.min.js"></script>
 <script>
-let prozorData = <?php echo $prozorDataset;?>
+    let sobnaData = <?php echo $sobnaData;?>;
+    let radijatorData = <?php echo $radijatorData;?>;
+    let klimaData = <?php echo $klimaData;?>;
+    let prozorData = <?php echo $prozorData;?>;
 </script>
 <script src="main-graph.js"></script>
