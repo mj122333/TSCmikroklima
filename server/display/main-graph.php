615dd8ea69df61db028a82a5607f84d71fb2d387
SuninDarknes <sunindark00@gmail.com>
2023-01-25 18:02:54 +0100

    Update main-graph.php

--- server/display/main-graph.php
+++ server/display/main-graph.php
@@ -4,10 +4,28 @@ if(!isset($_GET["prostorija"])){
     echo "U zahtjevu nema informacije o adresi.\n";
     exit();
 }
-echo $_GET["prostorija"];
 
-$sql_query = "SELECT * FROM PROSTORIJA";
+$sql_query = "SELECT * FROM PROSTORIJA WHERE NAZIV='".$_GET["prostorija"]."'";
 $result = mysqli_query($con, $sql_query);
-print_r(mysqli_fetch_array($result));
+$data = mysqli_fetch_array($result);
+echo $sql_query;
 echo "<br>";
+print_r($data);
+echo "<br>";
+$sql_query = "SELECT * FROM TEMP_SENZOR WHERE ID_CVOR=".$data["ID_CVOR"];
+$result = mysqli_query($con, $sql_query);
+echo $sql_query;
+echo "<br>";
+
+if($row["TIP"] == 0)$zrakID = $row["ID"];
+if($row["TIP"] == 1)$radijatorID = $row["ID"];
+if($row["TIP"] == 2)$kilmaID = $row["ID"];
+$klima="";
+while ($row = mysqli_fetch_array($result)){
+    if($row["TIP"] == 0)$zrakID = $row["ID"];
+    if($row["TIP"] == 1)$radijatorID = $row["ID"];
+    if($row["TIP"] == 2)$kilmaID = $row["ID"];
+}
+
+
 ?>
