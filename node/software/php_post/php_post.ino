a56a44da642c839d6804b540276f23e2f17cea6b
David Blazeka <122399526+DaviB45@users.noreply.github.com>
2023-03-22 08:37:27 +0100

    Update php_post.ino

--- node/software/php_post/php_post.ino
+++ node/software/php_post/php_post.ino
@@ -18,15 +18,19 @@ String zaSlanjeT = "";
 String zaSlanjeH = "";
 String zaSlanjeM = "";
 int broj_senzora = 0;
+bool led_stanje = LOW;
 bool zadnjeStanjeHall[3];
 bool statusObjekt[3];
 #define ONE_WIRE_BUS 13
 #define B20_POWER 23
 #define HALL_POWER 16
 #define HALL_READ1 17
-#define HALL_READ2 18
-#define HALL_READ3 19
+//#define HALL_READ2 18
+//#define HALL_READ3 19
 #define BATT 32
+#define RED_LED 5
+#define GREEN_LED 18
+#define BLUE_LED 19
 
 OneWire oneWire(ONE_WIRE_BUS);
 DallasTemperature sensors(&oneWire);
@@ -34,14 +38,18 @@ DeviceAddress Thermometer;
 
 void setup() {
     Serial.begin(115200);
+    pinMode(RED_LED, OUTPUT);
+    pinMode(GREEN_LED, OUTPUT);
+    pinMode(BLUE_LED, OUTPUT);
     WiFi.begin(ssid, password);
     delay(2000);
     Serial.println("Spajanje");
     while (WiFi.status() != WL_CONNECTED) {
         WiFi.begin(ssid, password);
-        delay(5000);
+        spajanje(5000);
         // yield();
     }
+    spojeno();
     Serial.println("");
     Serial.print("IP: ");
     Serial.println(WiFi.localIP());
@@ -51,8 +59,8 @@ void setup() {
     Serial.println(WiFi.RSSI());
     pinMode(HALL_POWER, OUTPUT);
     pinMode(HALL_READ1, INPUT_PULLUP);
-    pinMode(HALL_READ2, INPUT_PULLUP);
-    pinMode(HALL_READ3, INPUT_PULLUP);
+    //pinMode(HALL_READ2, INPUT_PULLUP);
+    //pinMode(HALL_READ3, INPUT_PULLUP);
     pinMode(BATT, INPUT);
     pinMode(B20_POWER, OUTPUT);
     digitalWrite(B20_POWER, HIGH);  // no deep sleep
@@ -76,17 +84,17 @@ void loop() {
 
 void hallRefresh() {                // void funkcija očitanja statusaObjekta
     digitalWrite(HALL_POWER, LOW);  // reset hall senzora
-    delay(10);
+    delay(100);
     digitalWrite(HALL_POWER, HIGH);
     delay(100);  // pricekati da se hallovi stabiliziraju?
     zaSlanjeH = "";
     zaSlanjeH = ",\"statusObjekt\" : {";
-    for (int i = 0; i < 3; i++) {
+    for (int i = 0; i < 3; i++) { //do 3 za 3 hall senzora
         statusObjekt[i] =
-            digitalRead(17 + i);  // 17, 18, 19 su ulazi senzora statusObjekt-a
+            digitalRead(17);  // 17, 18, 19 su ulazi senzora statusObjekt-a, obrisano + i jer se koristi samo jedan pin
         delay(20);
         if (statusObjekt[i] !=
-            digitalRead(17 + i))  // ako dva slijedna ocitanja daju razlicite
+            digitalRead(17))  // ako dva slijedna ocitanja daju razlicite
                                   // vrijednosti -> preskoci
             continue;
         /*
@@ -110,6 +118,7 @@ void hallRefresh() {                // void funkcija očitanja statusaObjekta
 
 void pushData() {
     if (WiFi.status() == WL_CONNECTED) {
+        spojeno();
         HTTPClient http;
 
         http.begin(serverName.c_str());
@@ -144,7 +153,7 @@ void pushData() {
                                 // slanja poruke (ponovno spajanje)
             Serial.println("LostConnection, SPAJANJE!");
             WiFi.begin(ssid, password);
-            delay(15000);  // esp32 se pokušava spojiti svakih 15 sekundi
+            spajanje(15000);  // esp32 se pokušava spojiti svakih 15 sekundi
             // yield();
         }
         pushData();  // slanje podataka odmah nakon ponovnog spajanja
@@ -152,12 +161,12 @@ void pushData() {
 }
 
 void get_metadata() {
-    zaSlanjeM = ",\"metadata\" : {\"wifi_rssi:\" : \"";
+    zaSlanjeM = ",\"metadata\" : {\"wifi_rssi\" : \"";
     zaSlanjeM += WiFi.RSSI();
     zaSlanjeM += "\",\"baterija\" : \"";
     float ulazA = analogRead(BATT);
     float napon = ulazA / 4096 * 6.6;  // 3.3V nominal, naponsko djelilo 1/2
-    zaSlanjeM += napon + 0.3;          //+0.3 kalibracija
+    zaSlanjeM += uint16_t((napon + 0.3)*1000);          //+0.3 kalibracija, mV conv
     zaSlanjeM += "\"}";
 }
 
@@ -191,4 +200,22 @@ String printAddress(
         adresa += String(deviceAddress[i], HEX);
     }
     return adresa;
+}
+
+void spajanje(int t){
+  digitalWrite(GREEN_LED, HIGH);
+  digitalWrite(BLUE_LED, HIGH);
+  t = t/100;
+  for(int i = 0; i < t; i++){
+    digitalWrite(RED_LED, led_stanje);
+    led_stanje = !led_stanje;
+    delay(100);
+  }  
+  digitalWrite(RED_LED, LOW);
+}
+
+void spojeno(){
+  digitalWrite(RED_LED, HIGH);
+  digitalWrite(GREEN_LED, LOW);
+  digitalWrite(BLUE_LED, LOW);
 }
